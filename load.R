837204921e7f2fd0ec341d688b9d1effd22078bd
Lucas Cherkewski <lucas@lucascherkewski.com>
2021-03-13 14:50:09 -0500

    progress! (lol commit messages)

--- load.R
+++ load.R
@@ -12,4 +12,52 @@ room_occupancies <- room_occupancies_raw %>%
   mutate(minute = as.POSIXct(as.numeric(minute) * 1440 * 60, origin = "2021-01-01", tz = "UTC")) %>%
   mutate(minute_fmt = strftime(minute, "%H:%M", tz = "UTC"))
 
-room_occupancies
+room_occupancies_raw %>%
+  clean_names %>%
+  select(floor_id:space_class, -day) %>%
+  mutate(row_number = row_number()) %>%
+  mutate(date = as_date(date)) %>%
+  select(-floor_id, -room_name, -space_class) %>% ## TODO: remove later
+  rename(
+    timestamp = x24h_time,
+    occupied_status = y_enum
+  ) %>%
+  mutate(
+    # timestamp_raw = timestamp,
+    timestamp = round_date(ymd_hms(paste0(date, timestamp), tz = "UTC"), "1 minute"),
+  ) %>%
+  group_by(sensor_id, date) %>%
+  mutate(
+    next_timestamp = lead(timestamp)
+  ) %>%
+  mutate(
+    next_timestamp = if_else(
+      timestamp == next_timestamp,
+      next_timestamp,
+      next_timestamp - minutes(1)
+    )
+  ) %>%
+  mutate(
+    next_timestamp = if_else(
+      is.na(next_timestamp),
+      ceiling_date(timestamp, "1 day") - minutes(1),
+      next_timestamp
+    )
+  ) %>%
+  # filter(570 <= row_number, row_number <= 575) %>%
+  group_by(row_number) %>%
+  mutate(
+    minute = map2(timestamp, next_timestamp, seq, by = "1 min")
+  ) %>%
+  unnest(minute)
+
+
+ts_start
+ts_end
+
+seq(ts_start, ts_end, by = "1 min")
+
+as_datetime("2020-01-02 00:01:00")
+as_datetime("2020-01-02 00:01:00")
+
+
