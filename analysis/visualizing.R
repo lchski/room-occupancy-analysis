7531c7dedd31ebe2b4550d265a687b281ee4bf0c
Lucas Cherkewski <lucas@lucascherkewski.com>
2021-03-21 20:33:27 -0400

    toying with some visualizations

--- analysis/visualizing.R
+++ analysis/visualizing.R
@@ -1,3 +1,5 @@
+library(slider)
+
 zzz <- room_occupancy_minutes %>%
   #filter(sensor_id %in% sensor_ids_to_process) %>%
   select(-timestamp, -next_timestamp) %>%
@@ -9,8 +11,13 @@ zzz <- room_occupancy_minutes %>%
   rename(hour_minute_fmt = hour_minute) %>%
   mutate(hour_minute = ymd_hm(paste0("1970-01-01T", hour_minute_fmt), tz = "UTC"))
 
-zzz2 %>%
+zzz %>%
   ungroup %>%
   filter(is_occupied) %>%
-  ggplot(aes(hour_minute, prop)) +
-  geom_point()
+  mutate(prop_ma = slide_dbl(prop, mean, .before = 7, .after = 7)) %>%
+  pivot_longer(c(prop, prop_ma)) %>%
+  ggplot(aes(hour_minute, value, color = name)) +
+  geom_point() +
+  facet_grid(vars(name)) +
+  scale_x_datetime(date_labels = "%H:%M", date_breaks = "1 hour") +
+  theme(axis.text.x = element_text(angle = 90))
